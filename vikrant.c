e5f5c103c862092635fc8e1bcda6f44420ff68aa
vigneshveera5073 <vigneshveera5073@gmail.com>
2024-05-19 15:17:09 +0530

    new update

--- vikrant.c
+++ vikrant.c
@@ -4,10 +4,10 @@ int main()
 {
     int num1,num2,num3;
     float average,add;
-    printf("enter a first number : ");
+    printf("enter any three numbers : ");
     scanf("%d%d%d",&num1,&num2,&num3);
     add=num1+num2+num3;
     average=add/3;
-    printf("you have enter this number : %f",average);
+    printf("average is : %f",average);
     return 0;
 }
